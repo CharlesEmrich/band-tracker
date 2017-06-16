4950ba886fad7d93bc53c680babc58bd1eca7cc7
Charles Emrich <libertinistic@gmail.com>
2017-06-16 11:19:34 -0700

    add code to Band to pass all CRUD tests

---
+++ Objects/Band.cs
@@ -0,0 +1,173 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace BandTracker.Objects
+{
+  public class Band
+  {
+    public int Id  { get; set; }
+    public string Name { get; set; }
+
+    public Band(string name, int id = 0)
+    {
+      Id = id;
+      Name = name;
+    }
+    public override bool Equals(System.Object otherBand)
+    {
+        if (!(otherBand is Band))
+        {
+          return false;
+        }
+        else
+        {
+          Band newBand = (Band) otherBand;
+          bool idEquality = (this.Id == newBand.Id);
+          bool nameEquality = (this.Name == newBand.Name);
+          return (idEquality && nameEquality);
+        }
+    }
+
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO bands (name) OUTPUT INSERTED.id VALUES (@BandName);", conn);
+
+      SqlParameter nameParameter = new SqlParameter("@BandName", this.Name);
+      cmd.Parameters.Add(nameParameter);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this.Id = rdr.GetInt32(0);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if(conn != null)
+      {
+        conn.Close();
+      }
+    }
+
+    public static List<Band> GetAll()
+    {
+      List<Band> allBands = new List<Band>{};
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM bands;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+      while(rdr.Read())
+      {
+        int bandId = rdr.GetInt32(0);
+        string bandName = rdr.GetString(1);
+        Band newBand = new Band(bandName, bandId);
+        allBands.Add(newBand);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return allBands;
+    }
+
+    public static Band Find(int id)
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM bands WHERE id = @BandId;", conn);
+      SqlParameter bandIdParameter = new SqlParameter();
+      bandIdParameter.ParameterName = "@BandId";
+      bandIdParameter.Value = id.ToString();
+      cmd.Parameters.Add(bandIdParameter);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      int foundBandId = 0;
+      string foundBandName = null;
+      while(rdr.Read())
+      {
+        foundBandId = rdr.GetInt32(0);
+        foundBandName = rdr.GetString(1);
+      }
+      Band foundBand = new Band(foundBandName, foundBandId);
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return foundBand;
+    }
+
+    public void Update(string newName)
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("UPDATE bands SET name = @NewName OUTPUT INSERTED.name WHERE id = @BandId;", conn);
+      SqlParameter newNameParameter = new SqlParameter("@NewName", newName);
+      cmd.Parameters.Add(newNameParameter);
+      SqlParameter bandIdParameter = new SqlParameter("@BandId", this.Id);
+      cmd.Parameters.Add(bandIdParameter);
+
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this.Name = rdr.GetString(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+    public void Delete()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("DELETE FROM bands WHERE id = @BandId;", conn);
+
+      SqlParameter bandIdParameter = new SqlParameter();
+      bandIdParameter.ParameterName = "@BandId";
+      bandIdParameter.Value = this.Id;
+
+      cmd.Parameters.Add(bandIdParameter);
+      cmd.ExecuteNonQuery();
+
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM bands;", conn);
+      cmd.ExecuteNonQuery();
+
+      cmd = new SqlCommand("DELETE FROM bands_venues;", conn);
+      cmd.ExecuteNonQuery();
+      conn.Close();
+    }
+  }
+}
