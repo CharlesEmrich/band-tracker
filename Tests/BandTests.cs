95c6fe09c3ad0ac9f1514f5dbd1ca34381c3cc33
Charles Emrich <libertinistic@gmail.com>
2017-06-16 11:17:28 -0700

    add failing tests for Band CRUD ops

---
+++ Tests/BandTests.cs
@@ -0,0 +1,105 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using BandTracker.Objects;
+
+namespace BandTracker
+{
+  [Collection("BandTrackerTests")]
+  public class BandTest : IDisposable
+  {
+    public BandTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=band_tracker_test;Integrated Security=SSPI;";
+    }
+    public void Dispose()
+    {
+      Band.DeleteAll();
+    }
+    [Fact]
+    public void Band_Equal_ReturnsTrueForIdenticalObjects()
+    {
+      //Arrange, Act
+      Band firstBand = new Band("David Bowie");
+      Band secondBand = new Band("David Bowie");
+      //Assert
+      Assert.Equal(firstBand, secondBand);
+    }
+    [Fact]
+    public void Band_BandsEmptyAtFirst()
+    {
+      //Arrange, Act
+      int result = Band.GetAll().Count;
+      //Assert
+      Assert.Equal(0, result);
+    }
+    [Fact]
+    public void Band_Save_SavesBandToDatabase()
+    {
+      //Arrange
+      Band testBand = new Band("Lightning Bolt");
+      testBand.Save();
+      //Act
+      List<Band> result = Band.GetAll();
+      List<Band> testList = new List<Band>{testBand};
+      //Assert
+      Assert.Equal(testList, result);
+    }
+    [Fact]
+    public void Band_Find_FindsBandInDatabase()
+    {
+      //Arrange
+      Band testBand = new Band("Kazumoto Endo");
+      testBand.Save();
+      //Act
+      Band foundBand = Band.Find(testBand.Id);
+      //Assert
+      Assert.Equal(testBand, foundBand);
+    }
+    [Fact]
+    public void Band_Update_UpdatesBandNameInDatabase()
+    {
+      //Arrange
+      Band testBand = new Band("Farmakon");
+      testBand.Save();
+      string newName = "Pharmakon";
+      //Act
+      testBand.Update(newName);
+      string result = testBand.Name;
+      //Assert
+      Assert.Equal(newName, result);
+    }
+    [Fact]
+    public void Band_Delete_RemovesBandFromDatabase()
+    {
+      //Arrange
+      Band testCase1 = new Band("Nick Cave");
+      testCase1.Save();
+      Band testCase2 = new Band("Lou Reed");
+      testCase2.Save();
+      //Act
+      testCase1.Delete();
+      int actual = Band.GetAll().Count;
+      int expected = 1;
+      //Assert
+      Assert.Equal(expected, actual);
+    }
+    [Fact]
+    public void Band_DeleteAll_RemovesAllBandsFromDatabase()
+    {
+      //Arrange
+      Band testCase1 = new Band("Carpenter Brut");
+      testCase1.Save();
+      Band testCase2 = new Band("Aphex Twin");
+      testCase2.Save();
+      //Act
+      Band.DeleteAll();
+      int actual = Band.GetAll().Count;
+      int expected = 0;
+      //Assert
+      Assert.Equal(expected, actual);
+    }
+  }
+}
