0ea304e361971420f7f9d9fcc2f2feb9d683d056
Charles Emrich <libertinistic@gmail.com>
2017-06-16 14:29:10 -0700

    add preliminary code for detail views and associating bands and venues

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -17,30 +17,40 @@ namespace MusicianTracker
       Get["/venues"] = _ => {
         return View["venues.cshtml", Venue.GetAll()];
       };
-      // Get["/clients"] = _ => {
-      //   Dictionary<string, object> model = new Dictionary<string, object> {{"clients", Client.GetAll()}, {"stylists", Stylist.GetAll()}};
-      //   return View["clients.cshtml", model];
-      // };
-      // Get["/stylists"] = _ => {
-      //   Dictionary<string, object> model = new Dictionary<string, object> {{"clients", Client.GetAll()}, {"stylists", Stylist.GetAll()}};
-      //   return View["stylists.cshtml", model];
-      // };
       // Get["/clients/edit/{id}"] = parameters => {
       //   Client foundClient = Client.Find(parameters.id);
       //   return View["client-edit.cshtml", foundClient];
       // };
-      // Get["/clients/view/{id}"] = parameters => {
-      //   Client foundClient = Client.Find(parameters.id);
-      //   return View["client-details.cshtml", foundClient];
-      // };
-      // Get["/stylists/view/{id}"] = parameters => {
-      //   Stylist foundStylist = Stylist.Find(parameters.id);
-      //   return View["stylist-details.cshtml", foundStylist];
-      // };
+      Get["/bands/{id}"] = parameters => {
+        Band foundBand = Band.Find(parameters.id);
+        return View["band-details.cshtml", foundBand];
+      };
+      Get["/venues/{id}"] = parameters => {
+        Venue foundVenue = Venue.Find(parameters.id);
+        return View["venue-details.cshtml", foundVenue];
+      };
       Post["/bands/new"] = _ => {
         Band newBand = new Band(Request.Form["name"]);
         newBand.Save();
         return View["bands.cshtml", Band.GetAll()];
+      };
+      Post["/bands/{id}/new"] = _ => {
+        //TODO: Make this route and its complementary venue route better at distinuishing new venues and existing ones.
+        Venue newVenue = new Venue(Request.Form["name"]);
+        newVenue.Save();
+        Band.Find(parameters.id).AddVenue(newVenue);
+        return View["band-details.cshtml", Band.Find(parameters.id)];
+      };
+      Post["/venues/new"] = _ => {
+        Venue newVenue = new Venue(Request.Form["name"]);
+        newVenue.Save();
+        return View["venues.cshtml", Venue.GetAll()];
+      };
+      Post["/venues/{id}/new"] = _ => {
+        Band newBand = new Band(Request.Form["name"]);
+        newBand.Save();
+        Venue.Find(parameters.id).AddBand(newBand);
+        return View["venue-details.cshtml", Venue.Find(parameters.id)];
       };
       // Post["/stylists/new"] = _ => {
       //   Stylist newStylist = new Stylist(Request.Form["stylist-name"]);
